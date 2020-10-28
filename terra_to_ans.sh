0cde8ff6967ba98cfdc83b0c327fb3104ce7dba4
integrator <integrat.net@gmail.com>
2020-10-28 22:23:53 +0300

    terra_to_ans delete blank lines

--- terra_to_ans.sh
+++ terra_to_ans.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /home/nonroot/terra/test2
-terraform output public_ip | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5$
+terraform output public_ip | awk -F\" '{print $2}'  > /etc/ansible/aws_ip
 
-i=`head -n 1 /etc/ansible/aws_ip.txt | tail -n 1`
+i=`egrep -v '^#|^$' /etc/ansible/aws_ip  |  head -n 1 | tail -n 1`
 
 echo -en "[MAVEN]\n$i\n" > /etc/ansible/hosts
 
-i=`head -n 2 /etc/ansible/aws_ip.txt | tail -n 1`
+i=`egrep -v '^#|^$' /etc/ansible/aws_ip  |  head -n 2 | tail -n 1`
 
 echo -en "[TOMCAT]\n$i\n" >> /etc/ansible/hosts
