fa6c5b7a9ceb3416737e8f8e3e0803ed21abf051
integrator <integrat.net@gmail.com>
2020-10-29 15:12:38 +0300

    ready...

--- terra_to_ans.sh
+++ terra_to_ans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/nonroot/terra/test2
+#cd /home/nonroot/terra/test2
 terraform output public_ip | awk -F\" '{print $2}'  > /etc/ansible/aws_ip
 
 i=`egrep -v '^#|^$' /etc/ansible/aws_ip  |  head -n 1 | tail -n 1`
