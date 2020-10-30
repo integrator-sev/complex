ff740d68e20c44f8d86b15fec2f9101bf8d7ba48
integrator <integrat.net@gmail.com>
2020-10-30 12:59:40 +0300

    ansible hosts in workspace

--- terra_to_ans.sh
+++ terra_to_ans.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #cd /home/nonroot/terra/test2
-terraform output public_ip | awk -F\" '{print $2}'  > /etc/ansible/aws_ip
+terraform output public_ip | awk -F\" '{print $2}'  > aws_ip
 
-i=`egrep -v '^#|^$' /etc/ansible/aws_ip  |  head -n 1 | tail -n 1`
+i=`egrep -v '^#|^$' aws_ip  |  head -n 1 | tail -n 1`
 
-echo -en "[MAVEN]\n$i\n" > /etc/ansible/hosts
+echo -en "[MAVEN]\n$i\n" > hosts
 
-i=`egrep -v '^#|^$' /etc/ansible/aws_ip  |  head -n 2 | tail -n 1`
+i=`egrep -v '^#|^$' aws_ip  |  head -n 2 | tail -n 1`
 
-echo -en "[TOMCAT]\n$i\n" >> /etc/ansible/hosts
+echo -en "[TOMCAT]\n$i\n" >> hosts
