08cbc131737ec22bf9fe4968f1d5a42b178626df
integrator <integrat.net@gmail.com>
2020-11-01 23:12:20 +0300

    wait for ssh

--- config.tf
+++ config.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "ssh_access" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 egress {
     from_port = 0
     to_port   = 0
